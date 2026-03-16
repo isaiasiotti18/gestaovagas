23c4526d5536bcc51b9279d2c1c800f07b713481
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2026-03-16 18:06:06 -0300

    refactor: Update JobController to use CreateJobDTO and extract company ID from request

--- src/main/java/com/isaiasiotti/gestaovagas/modules/company/controllers/JobController.java
+++ src/main/java/com/isaiasiotti/gestaovagas/modules/company/controllers/JobController.java
@@ -3,10 +3,14 @@ package com.isaiasiotti.gestaovagas.modules.company.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.isaiasiotti.gestaovagas.modules.company.dtos.CreateJobDTO;
 import com.isaiasiotti.gestaovagas.modules.company.entities.JobEntity;
 import com.isaiasiotti.gestaovagas.modules.company.useCases.CreateJobUseCase;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +27,20 @@ public class JobController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         try {
+            var companyId = request.getAttribute("company_id");
+
+            // jobEntity.setCompanyId(UUID.fromString(companyId.toString()));
+
+            var jobEntity = JobEntity
+                    .builder()
+                    .companyId(UUID.fromString(companyId.toString()))
+                    .benefits(createJobDTO.getBenefits())
+                    .description(createJobDTO.getDescription())
+                    .level(createJobDTO.getLevel())
+                    .build();
+
             var result = this.createJobUseCase.execute(jobEntity);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
