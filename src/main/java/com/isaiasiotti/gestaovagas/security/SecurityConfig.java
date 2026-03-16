76a709c40e3d0f24e5a3d17e34eddecc0e4c014d
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2026-03-16 18:06:14 -0300

    feat: Update SecurityConfig with authorization rules and security filter integration

--- src/main/java/com/isaiasiotti/gestaovagas/security/SecurityConfig.java
+++ src/main/java/com/isaiasiotti/gestaovagas/security/SecurityConfig.java
@@ -3,14 +3,41 @@ package com.isaiasiotti.gestaovagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
     DefaultSecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
+            auth
+                    .requestMatchers("/candidate/")
+                    .permitAll()
+                    .requestMatchers("/company/")
+                    .permitAll()
+                    .requestMatchers("/auth/company")
+                    .permitAll()
+                    .requestMatchers("/error")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
+        }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
